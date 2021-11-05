0e379f49a31d95b5435751b18ee4d4ad2dee26b5
Artem Merets <info@appsec.team>
2021-11-05 12:26:36 +0300

    first commit

---
+++ src/main/java/securityboxes/riskbox/RiskboxApplication.java
@@ -0,0 +1,13 @@
+package securityboxes.riskbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskboxApplication.class, args);
+	}
+
+}
